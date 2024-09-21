ea65751eae4b81bac73f93d98da17c3a05927b54
Rokorori <rhtjdals1723@naver.com>
2024-09-21 22:07:02 +0900

    2nd commit

---
+++ opengl_project_3.cpp
@@ -0,0 +1,100 @@
+﻿#include <gl/glut.h>
+
+void draw_point(void);
+void draw_line(void);
+void draw_triangle(void);
+void draw(void);
+void idle(void);
+void init(void);
+
+// 전역변수
+double spin;
+
+int main(int argc, char** argv) {
+
+	/* GLUT 및 Window 초기화 */
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(300, 300);
+	glutCreateWindow("My First GL Program");
+	init();	 /* 사용자 초기화 함수 */
+
+	/* Callback 함수 정의 */
+	glutDisplayFunc(draw);	/* draw() -> 실제 그리기 함수 */
+	glutIdleFunc(idle);
+	/* Looping 시작 */
+	glutMainLoop();
+
+	return 0;
+}
+void draw_point(void){
+	/* 점의 색상을 빨간색으로 설정 */
+	glColor3f(1.0f, 0.0f, 0.0f);
+	/* 점의 크기를 설정 (초기값은 1.0) */
+	glPointSize(4.0f);
+
+	glBegin(GL_POINTS);
+	glVertex2i(5, 5);
+	glVertex2i(200, 200);
+	glVertex2i(450, 300);
+	glEnd();
+}
+
+void draw_line(void){
+	/* 선의 색상을 노란색으로 설정 */
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glBegin(GL_LINES);
+	glVertex2i(50, 50);
+	glVertex2i(250, 450);
+	glVertex2i(300, 450);
+	glVertex2i(400, 100);
+	glEnd();
+}
+
+void draw_triangle(void){
+	/* 면의 색상을 노란색으로 설정 */
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glBegin(GL_TRIANGLES);
+	glVertex2i(50, 50);
+	glVertex2i(250, 450);
+	glVertex2i(400, 100);
+	glEnd();
+}
+
+void draw(void){
+	/* 화면을 깨끗하게 지우기 */
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	// 행렬 변환시 실제 적용 역순으로 사용
+	glTranslatef(250, 250, 0); // 평행이동 (x, y, z)
+	glRotatef(spin, 0, 0, 1); // 회전 (회전각, x, y, z)
+	glTranslatef(-250, -250, 0); // 크기변화 (x, y, z) 
+
+	/* 사용자 그리기 함수 */
+	//draw_point();			/* 점 그리기 */
+	//draw_line();			/* 선 그리기 */
+	draw_triangle();	/* 면 그리기 */
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+void idle(void) {
+	spin = spin + 0.1;
+	if (spin > 360) spin -= 360;
+	glutPostRedisplay(); // 화면 새로고침 요청
+}
+
+void init(void){
+	/* 화면의 기본색으로 Blue 설정 */
+	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+
+	/* 화면 좌표 정보 설정 */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0f, 500.0f, 0.0f, 500.0f);
+}
---
+++ opengl_project_3_task.cpp
@@ -0,0 +1,99 @@
+﻿/* read.me
+ * 
+ * 과제목표
+ * 1) 평행 이동 함수 사용 o
+ * 2) 회전 이동 함수 사용 o
+ * 3) 크기 변화 함수 사용 o
+ * 4) 간단한 애니메이션 구현 o
+ * 
+ * 추가구현
+ * 회전축을 기준으로 회오리를 그리며 크기변화 되도록 구현
+ * 크기 1.5 ~ 0배까지 커졌다 작아졌다 반복
+ */ 
+#include <gl/glut.h>
+
+// 함수
+void draw_triangle(void);
+void draw(void);
+void idle(void);
+void init(void);
+
+// 전역변수
+double spin(0); // 회전 전역변수
+double scale(1), scale_con(0.01); // 크기 전역변수
+
+int main(int argc, char** argv) {
+
+	/* GLUT 및 Window 초기화 */
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(300, 300);
+	glutCreateWindow("openGL_task_3");
+	init();	 /* 사용자 초기화 함수 */
+
+	/* Callback 함수 정의 */
+	glutDisplayFunc(draw);
+	glutIdleFunc(idle);
+	/* Looping 시작 */
+	glutMainLoop();
+
+	return 0;
+}
+
+void draw_triangle(void) {
+	
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 1.0f, 1.0f); // 민트
+	glVertex2i(350, 200);
+	glColor3f(1.0f, 1.0f, 0.0f); // 노랑
+	glVertex2i(350, 300);
+	glColor3f(1.0f, 0.0f, 1.0f); // 빨강
+	glVertex2i(425, 250);
+	glEnd();
+}
+
+void draw(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	glTranslatef(250, 250, 0); // 평행이동 (x, y, z)
+	glRotatef(spin, 0, 0, 1); // 회전 (회전각, x, y, z)
+	glScalef(scale, scale, scale); // 크기변화 (x, y, z)
+	glTranslatef(-250, -250, 0); // 평행이동 (x, y, z)
+	
+	draw_triangle(); // 삼각형 그리기
+	
+	glutSwapBuffers();
+}
+
+void idle(void) {
+	// 회전
+	spin = spin + 2.5;
+	if (spin > 360) {
+		spin -= 360;
+	}
+	
+	// 크기
+	scale = scale - scale_con;
+	if (scale > 1.5) {
+		scale_con = -scale_con;
+	}
+	
+	if (scale < 0) {
+		scale_con = -scale_con;
+	}
+	glutPostRedisplay();
+}
+
+void init(void) {
+	/* 화면의 기본색으로 Blue 설정 */
+	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+
+	/* 화면 좌표 정보 설정 */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0f, 500.0f, 0.0f, 500.0f);
+}
